1d0beb66763f437b19d0b8a68082eb359c47adc3
konglx <konglx90@gmail.com>
2020-06-12 17:46:46 +0800

    deploy:mini css file

--- config/less.js
+++ config/less.js
@@ -3,13 +3,18 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = () => [
     {
         test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-            fallback: "style-loader",
-            use: "css-loader"
-        })
+        use: ['style-loader', 'css-loader']
     },
     {
         test: /\.less$/,
-        use: ExtractTextPlugin.extract(['style-loader', 'css-loader', 'less-loader']),
+        use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: [{
+                loader: 'css-loader',
+                options: {
+                    minimize: true,
+                }
+            }, 'less-loader']
+        }),
     },
 ];
