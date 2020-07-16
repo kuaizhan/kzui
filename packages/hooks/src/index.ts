734874657676232c1a560eac92e910ec0904b3cb
Qiu Xing <children24563736@163.com>
2020-07-16 10:19:53 +0800

    feat: in progress

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,7 +1,10 @@
 import { useHover } from './useHover'
 import { usePrevious } from './usePrevious'
+import { useRequest, createUseRequest } from './useRequest';
 
 export {
   useHover,
-  usePrevious
+  usePrevious,
+  useRequest,
+  createUseRequest
 }
