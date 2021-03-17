eb007ceef9c7d091d46b787b598275c37c4f6cbc
qiuxing <qiuxing@icloudfuture.com>
2021-03-17 14:25:31 +0800

    feat: fix silly mistake

--- packages/kzui/src/components/select/index.tsx
+++ packages/kzui/src/components/select/index.tsx
@@ -232,11 +232,11 @@ class Select extends KZUIComponent<SelectProps, SelectStates> {
                     visible={expand}
                     onVisibleChange={visible => this.setState({ expand: visible })}
                     trigger='click'
-                    tipStyle={{ padding: 0, width: style.width || 200, minWidth: 'auto' }}
+                    tipStyle={{ padding: 0, width: style?.width || 200, minWidth: 'auto' }}
                     theme='light'
                     style={{ width: "100%", height: '100%' }}
                     tip={(
-                        <div className={optionsPanelCls} style={{ ...finalPopoverStyle, display: expand ? 'block' : 'none', width: style.width || 200 }}>
+                        <div className={optionsPanelCls} style={{ ...finalPopoverStyle, display: expand ? 'block' : 'none', width: style?.width || 200 }}>
                             <div className={`${clsPrefix}-options`}>
                                 {options?.map((option, index) => (
                                     <Option
